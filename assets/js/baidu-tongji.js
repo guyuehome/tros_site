1250e9c7fb2889e906c97f0815ccd929313c0467
Hu Chunxu <huchunxu@ps-micro.com>
2024-05-07 12:54:13 +0800

    Add files

---
+++ assets/js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3a07a93bb03e04fa7409cfc07b361413";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
